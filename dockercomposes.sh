4a0106a5365f8ee74844e92967a1e70a10f3c0da
daniel-bss <daniel.simamora2000@gmail.com>
2025-11-29 10:12:04 +0700

    chore: setup docker composes and bash script

---
+++ dockercomposes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ "$1" != "up" && "$1" != "upbuild" && "$1" != "down" ]]; then
+    echo "Unknown argument provided: $1"
+    exit 1
+fi
+
+yamls=(
+    "./internal/auth/docker-compose.yaml"
+    "./internal/z/docker-compose.yaml"
+)
+
+for yaml in "${yamls[@]}"; do
+    if [ "$1" == "up" ]; then
+        sudo docker compose -f "$yaml" up -d
+    elif [ "$1" == "upbuild" ]; then
+        sudo docker compose -f "$yaml" up --build -d
+    elif [ "$1" == "down" ]; then
+        sudo docker compose -f "$yaml" down
+    else
+        echo "Unknown argument provided: $1"
+    fi
+done
