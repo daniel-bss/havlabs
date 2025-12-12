e2012093df0d5caadf2bdd4ee81f9674bb75f0f2
daniel-bss <daniel.simamora2000@gmail.com>
2025-12-12 16:17:31 +0700

    feat: envoy now extract cookies into headers

--- internal/news/server/server.go
+++ internal/news/server/server.go
@@ -28,9 +28,8 @@ func NewGRPCService(config utils.Config) (*Server, error) {
 func (server *Server) GetNews(ctx context.Context, req *pb.NewsRequest) (*pb.NewsResponse, error) {
 	// mtdt := server.extractMetadata(ctx)
 	if md, ok := metadata.FromIncomingContext(ctx); ok {
-		fmt.Println("HEHE")
 		fmt.Println(md)
 	}
 	fmt.Println()
-	return &pb.NewsResponse{Title: "HEHE"}, nil
+	return &pb.NewsResponse{Title: "TEST"}, nil
 }
